d2aec0ac7b6f294c9cee2e7dcdd6fa1d2783d70e
Gbrvi <gv.almeida06@gmail.com>
2024-10-25 15:16:56 -0300

    Criado controller e View de equipment

---
+++ Reservation/Controllers/Equipment.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reservation.Controllers
+{
+    public class Equipment : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Reservation/obj/Debug/net8.0/Reservation.AssemblyInfo.cs
+++ Reservation/obj/Debug/net8.0/Reservation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171dbfdc9ba900f37f0f779d90aebdd72f121b50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f187d916b80ad155341a0fd71c43fc9b7bda2a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
