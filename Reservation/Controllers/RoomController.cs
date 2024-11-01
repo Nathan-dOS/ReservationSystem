7e39cfcb2e6967ea53950749326b53704e2174c8
Gbrvi <gv.almeida06@gmail.com>
2024-11-01 14:43:02 -0300

    Criado opção de editar rooms

--- Reservation/Controllers/RoomController.cs
+++ Reservation/Controllers/RoomController.cs
@@ -2,6 +2,9 @@
 using Reservation.Data;
 using Reservation.Interfaces;
 using Reservation.Models;
+using Reservation.ViewModel;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Reservation.Controllers
 {
@@ -16,7 +19,7 @@ namespace Reservation.Controllers
 
         public async Task<IActionResult> Index()
         {
-           IEnumerable<Room> rooms = await _roomRepository.GetAllRooms();
+            IEnumerable<Room> rooms = await _roomRepository.GetAllRooms();
 
             return View(rooms);
         }
@@ -43,6 +46,61 @@ namespace Reservation.Controllers
 
             return RedirectToAction("Index");
 
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var room = await _roomRepository.GetByIdAsync(id);
+            if (room == null)
+            {
+                return View("Error");
+            }
+
+            var roomVM = new EditRoomViewModel
+            {
+                IdRoom = room.IdRoom,
+                Number = room.Number,
+                Capacity = room.Capacity,
+                Size = room.Size,
+                Internet = room.Internet,
+                Status = room.Status,
+                Price = room.Price,
+                PhotoAlbum = room.PhotoAlbum,
+                SecurityCamera = room.SecurityCamera,
+                AirConditioning = room.AirConditioning,
+                Type = room.Type
+            };
+
+            return View(roomVM);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, EditRoomViewModel roomVM)
+        {
+            if(!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit Room");
+                return View("Edit", roomVM);
+            }
+
+            var room = new Room
+            {
+                IdRoom = id,
+                Number = roomVM.Number,
+                Capacity = roomVM.Capacity,
+                Size = roomVM.Size,
+                Internet = roomVM.Internet,
+                Status = roomVM.Status,
+                Price = roomVM.Price,
+                PhotoAlbum = roomVM.PhotoAlbum,
+                SecurityCamera = roomVM.SecurityCamera,
+                AirConditioning = roomVM.AirConditioning,
+                Type = roomVM.Type
+            };
+
+            _roomRepository.Update(room);
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Reservation/ViewModel/EditRoomViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Reservation.Data.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace Reservation.ViewModel
+{
+    public class EditRoomViewModel
+    {
+        public int IdRoom { get; set; }
+
+        public string Number { get; set; }
+
+        public int Capacity { get; set; }
+
+        public float Size { get; set; }
+
+        public RoomStatus Status { get; set; }
+        
+        public float Price { get; set; }
+
+        public string PhotoAlbum { get; set; }  // Caminho do álbum de fotos
+
+        public bool Internet { get; set; }
+
+        public bool SecurityCamera { get; set; }
+
+        [Required]
+        public bool AirConditioning { get; set; }
+
+        [Required]
+        public RoomType Type { get; set; }
+    }
+}
+
