4bca60f47a936ab354036795d13cf5d3a1925afc
Gbrvi <gv.almeida06@gmail.com>
2024-11-30 09:41:15 -0300

    Enviando Migrations

---
+++ Reservation/Migrations/20241113221238_RemovedAttributesFromReserveAndUser.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservation.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedAttributesFromReserveAndUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReservePhotos",
+                table: "Reserves");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Reserves",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Reserves",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReservePhotos",
+                table: "Reserves",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
